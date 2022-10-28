5e28731f6799c8b4266360d1441d3c08d5e16d44
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-10-27 20:38:20 -0600

    add very basic keybinds and player movement

--- main.py
+++ main.py
@@ -10,17 +10,35 @@ pygame.display.set_icon(icon)
 player_image = pygame.image.load('ship.png')
 playerX = 640
 playerY = 360
+playerX_change = 0
+playerY_change = 0
 
-def player():
-    screen.blit(player_image, (playerX, playerY))
+def player(x, y):
+    screen.blit(player_image, (x, y))
 
 game_running = True
 while game_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                playerY_change = -0.1
+            if event.key == pygame.K_DOWN:
+                playerY_change = 0.1
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.1
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                playerY_change = 0
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+                playerX_change = 0
 
     screen.fill((0, 0, 0))
-    player()
+    playerX += playerX_change
+    playerY += playerY_change
+    player(playerX, playerY)
 
     pygame.display.update()
