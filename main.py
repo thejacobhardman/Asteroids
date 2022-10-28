01f924c1436a2b59ff7209b50fae971c1565a846
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-10-27 20:21:00 -0600

    create player and add player image

--- main.py
+++ main.py
@@ -7,9 +7,20 @@ pygame.display.set_caption("Asteroids")
 icon = pygame.image.load('Asteroid Brown.png')
 pygame.display.set_icon(icon)
 
+player_image = pygame.image.load('ship.png')
+playerX = 640
+playerY = 360
+
+def player():
+    screen.blit(player_image, (playerX, playerY))
+
 game_running = True
 while game_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_running = False
 
+    screen.fill((0, 0, 0))
+    player()
+
+    pygame.display.update()
