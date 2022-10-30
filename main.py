1263513c782b04a6e261f2d69cab2c996b04675f
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-10-29 18:53:07 -0600

    implement screen resizing

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ MAX_SPEED = 9
 FPS = 60
 fps_clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption("Asteroids")
 icon = pygame.image.load('Asteroid Brown.png')
 pygame.display.set_icon(icon)
@@ -66,30 +66,62 @@ class Player(pygame.sprite.Sprite):
         if self.position.y > HEIGHT:
             self.position.y = 0
 
-class asteroid:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.radius = 80
+class Asteroid(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('Asteroid Brown.png')
-        self.x_change = 0
-        self.y_change = 0
+        self.original_image = self.image
+        self.position = vec(WIDTH / 2, HEIGHT / 2)
+        self.rect = self.image.get_rect(center=self.position)
+        self.vel = vec(0, 0)
 
-    def asteroid(self):
-        screen.blit(self.image, (self.x, self.y))
+    def update(self):
+        self.position += self.vel
+        self.rect.center = self.position
+        self.leave_screen()
 
-    # def update_location(self):
+    def leave_screen(self):
+        if self.position.x > WIDTH:
+            all_sprites.remove(self)
+        if self.position.x < 0:
+            all_sprites.remove(self)
+        if self.position.y <= 0:
+            all_sprites.remove(self)
+        if self.position.y > HEIGHT:
+            all_sprites.remove(self)
 
 all_sprites = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
 asteroid_count = 0
+fullscreen = False
 
 game_running = True
 while game_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_running = False
+        if event.type == pygame.VIDEORESIZE:
+            if not fullscreen:
+                WIDTH = event.w
+                HEIGHT = event.h
+                screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_f:
+                fullscreen = not fullscreen
+                if fullscreen:
+                    WIDTH = pygame.display.get_desktop_sizes()[0][0]
+                    HEIGHT = pygame.display.get_desktop_sizes()[0][1]
+                    screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
+                else:
+                    WIDTH = 1280
+                    HEIGHT = 720
+                    screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+
+    if asteroid_count <= 1:
+        asteroid = Asteroid()
+        all_sprites.add(asteroid)
+        asteroid_count += 1
 
     player.wrap_around_screen()
     all_sprites.update()
