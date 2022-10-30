5d8636d7570ed6cad8b99e721783972b7ec1b33a
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-10-29 18:06:53 -0600

    rebuild player into a class and implement rotational movement

--- main.py
+++ main.py
@@ -1,24 +1,70 @@
-import pygame, random
+import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((1280, 720))
+vec = pygame.math.Vector2
+
+WIDTH = 1280
+HEIGHT = 720
+MAX_SPEED = 9
+FPS = 60
+fps_clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Asteroids")
 icon = pygame.image.load('Asteroid Brown.png')
 pygame.display.set_icon(icon)
 
-player_image = pygame.image.load('ship.png')
-player_image.set_colorkey((0, 0, 0))
-playerX = 640
-playerY = 360
-playerX_change = 0
-playerY_change = 0
-player_angle_change = 0
-angle = 0
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('ship.png')
+        self.original_image = self.image
+        self.position = vec(WIDTH / 2, HEIGHT / 2)
+        self.rect = self.image.get_rect(center=self.position)
+        self.vel = vec(0, 0)
+        self.acceleration = vec(0, -0.2)
+        self.angle_speed = 0
+        self.angle = 0
 
-def player(x, y, angle):
-    image_copy = pygame.transform.rotate(player_image, angle)
-    screen.blit(pygame.transform.rotate(player_image, angle), (x - int(image_copy.get_width()/2), y - int(image_copy.get_height()/2)))
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.angle_speed = -4.5
+            player.rotate()
+        if keys[pygame.K_RIGHT]:
+            self.angle_speed = 4.5
+            player.rotate()
+        if keys[pygame.K_UP]:
+            self.vel += self.acceleration
+        if keys[pygame.K_DOWN]:
+            self.vel -= self.acceleration
+
+        if self.vel.length() > MAX_SPEED:
+            self.vel.scale_to_length(MAX_SPEED)
+
+        self.position += self.vel
+        self.rect.center = self.position
+
+    def rotate(self):
+        self.acceleration.rotate_ip(self.angle_speed)
+        self.angle += self.angle_speed
+        if self.angle > 360:
+            self.angle -= 360
+        elif self.angle < 0:
+            self.angle += 360
+        self.image = pygame.transform.rotate(self.original_image, -self.angle)
+        self.rect = self.image.get_rect(center=self.rect.center)
+
+    def wrap_around_screen(self):
+        if self.position.x > WIDTH:
+            self.position.x = 0
+        if self.position.x < 0:
+            self.position.x = WIDTH
+        if self.position.y <= 0:
+            self.position.y = HEIGHT
+        if self.position.y > HEIGHT:
+            self.position.y = 0
 
 class asteroid:
     def __init__(self, x, y):
@@ -34,51 +80,21 @@ class asteroid:
 
     # def update_location(self):
 
-
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 asteroid_count = 0
+
 game_running = True
 while game_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_running = False
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
-            playerY_change = -0.3
-        if keys[pygame.K_DOWN]:
-            playerY_change = 0.3
-        if keys[pygame.K_RIGHT]:
-            player_angle_change = -0.2
-        if keys[pygame.K_LEFT]:
-            player_angle_change = 0.2
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                playerY_change = 0
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
-                player_angle_change = 0
+
+    player.wrap_around_screen()
+    all_sprites.update()
 
     screen.fill((0, 0, 0))
-
-    playerX += playerX_change
-    playerY += playerY_change
-
-    # Keeps angle between 0 and 360 so it can be used for velocity calculations
-    angle += player_angle_change
-    if angle > 360:
-        angle = 0
-    if angle < 0:
-        angle = 360
-
-    if playerX <= 0:
-        playerX = 1235
-    if playerX > 1235:
-        playerX = 0
-    if playerY < 0:
-        playerY = 689
-    if playerY > 689:
-        playerY = 0
-
-    player(playerX, playerY, angle)
-
-    print(angle)
-
+    all_sprites.draw(screen)
     pygame.display.update()
+    fps_clock.tick(FPS)
